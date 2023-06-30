1b7f5153b0412da0b38a7ecb1d6b8a7a2c13edef
Yash Sharma <yash95kumar@gmail.com>
2023-06-29 17:18:57 -0700

    eda

--- projects/name_entry_logger/saveToFile.py
+++ projects/name_entry_logger/saveToFile.py
@@ -1,31 +1,47 @@
-#This file will take the name, age and address of the person and log it
-#if the data is already present we will throw an error
 
-import logging
 import os
+import logging
+
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+f=logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+
+fh=logging.FileHandler('saveTofile.log')
+fh.setFormatter(f)
+
+logger.addHandler(fh)
+
+#logging.basicConfig(filename='saveTofile.log',level=logging.DEBUG,format='%(asctime)s:%(levelname)s:%(message)s')
+
 
 def namecheck(name):
-    if os.path.exists("data.txt"):
-        with open("data.txt",'r') as readFile:
+    logger.debug(f'checking name "{name}"....')
+    if os.path.exists('data.txt'):
+        with open('data.txt','r') as readFile:
             for line in readFile:
-                if line.lower().startswith(f'Name: {name}'):
-                    logging.error(f"Name: {name} already exists")
+                if line.lower().startswith(f'name: {name.lower()}'):
+                    logger.error(f'Name: "{name}" already exists')
                     return False
-                if len(name) == 0:
-                    logging.critical("Name cannot be blank")
-                    return False
-                elif not name.isalpha():
-                    logging.critical("Name should be alphanumeric")
-                    return False
-                else:
-                    logging.error(f"check successful")
-                    return True
+            if len(name) == 0:
+                logger.critical('Name cannot be blank')
+                return False
+            elif not name.isalpha():
+                logger.critical('name must be an alphabet')
+                return False
+            else:
+                logger.error(f'check successfull')
+                return True
     else:
-        logging.debug('No data found')
-        return True      
+        logger.debug('No data found')
+        return True
 
-def saveData(name, age, email):
-    with open("data.txt", 'a') as appendFile:
-        appendFile.write(f"Name: {name} - Age: {age} - Email: {email}")
-        print("Details saved successfully")
 
+def saveData(name,age,email):
+    logger.debug(f'Saving details of {name}...')
+    with open('data.txt','a') as appendFile:
+        appendFile.write(f'Name: {name} - Age: {age} - Email: {email}\n')
+        print(f'Details saved successfully')
+
+logger.info('End of saveToFile Program')
+logger.debug('########################')
