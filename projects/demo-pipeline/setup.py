ff9e41adba7cff0b251e8c8144eb2891acadafc0
Yash Sharma <yash95kumar@gmail.com>
2023-07-06 23:07:25 -0700

    ,

---
+++ projects/demo-pipeline/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+setup(name = 'census-income', version = '0.0.1', author='yash',author_email = 'yash95kumar@gmail.com',package = find_packages()
+,install_requires = ["pandas", "numpy", "flask"])
