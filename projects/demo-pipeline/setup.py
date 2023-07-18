02100ba491e6abd8ea2759de5a15dc4317becda6
Yash Sharma <yash95kumar@gmail.com>
2023-07-18 14:12:37 -0700

    add setup.py file to create census-income package

--- projects/demo-pipeline/setup.py
+++ projects/demo-pipeline/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name = 'census-income', version = '0.0.1', author='yash',author_email = 'yash95kumar@gmail.com',package = find_packages()
-,install_requires = ["pandas", "numpy", "flask"])
+setup(name = 'census-income', 
+      version = '0.0.1', 
+      author='yash',
+      author_email = 'yash95kumar@gmail.com',
+      packages = find_packages(),
+      install_requires = ["pandas", "numpy", "flask"])
